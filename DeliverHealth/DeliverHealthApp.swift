3dcd9b4328eaca1e94e7c91f773bce0ce1084343
Daniel Espinosa <despinosa112@gmail.com>
2022-07-23 16:56:55 -0400

    Initial Commit

---
+++ DeliverHealth/DeliverHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverHealthApp.swift
+//  DeliverHealth
+//
+//  Created by Daniel Espinosa on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DeliverHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
